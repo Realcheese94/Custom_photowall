0e67a51299aad9094163a68cced4fc8d4cda08a6
Real cheese <dbtjdwocjswo@gmail.com>
2018-07-23 15:10:05 +0900

    Signed-off-by: Real cheese <dbtjdwocjswo@gmail.com>

--- post/views.py
+++ post/views.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Comment,Post
+from member.models import User
 
 
 def post_list(request):
-    posts = Post.object.all()
+    posts = Post.objects.all()
     context = {
         'posts' : posts
     }
 
-    return render(request,"post/post.html",context)
+    return render(request,"post.html",context)
 # Create your views here.
